72ae6c84ef761dc24e3a772cd57745062fc9a6a7
Tristan <tristandevaux6@icloud.com>
2025-05-12 13:38:07 +0200

    Enhance news display and modal functionality
    - Added support for multiple associated images in news items on the accueil.html template.
    - Removed debugging messages from the news display section for a cleaner UI.
    - Updated JavaScript to handle image display in the modal without relying on debug messages.
    - Minor formatting adjustments in the URLs.py and styles.css files for consistency.

--- apesf_site/urls.py
+++ apesf_site/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
 # Ajouter la gestion des fichiers médias (toujours actif, même en production pour le développement local)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-handler404 = 'apesf.views.erreur_404'
+handler404 = 'apesf.views.erreur_404' 
+
